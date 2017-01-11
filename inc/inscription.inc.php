be116d2f8212cb4b7477ef02d1f343611a6a6d84
oxsparks <paris.romain.pro@gmail.com>
2017-01-11 11:07:53 +0100

    add form inscription

--- inc/inscription.inc.php
+++ inc/inscription.inc.php
@@ -1,4 +1,15 @@
 <?php
 
  ?>
- 
+ <form action="index.html" method="post">
+  <input type="text" name="pseudo" placeholder="Votre pseudo">
+  <input type="password" name="mdp" placeholder="Votre mot de passe">
+  <input type="text" name="nom" placeholder="Votre Nom">
+  <input type="text" name="prenom" placeholder="Votre Prénom">
+  <input type="email" name="email" placeholder="Votre Email">
+  <select name="sexe">
+    <option value="Femme">Femme</option>
+    <option value="Homme">Homme</option>
+  </select>
+  <input type="submit" value="Inscription">
+ </form>
