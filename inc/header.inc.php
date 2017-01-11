b2aaa0545041fcde2ad219f4e795c0dd67e21acf
oxsparks <paris.romain.pro@gmail.com>
2017-01-11 10:55:25 +0100

    add inc

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -21,11 +21,9 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Espace Membre <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="inc/inscription.php">Inscription</a></li>
+            <li><a href="#">Connexion</a></li>
+            <li><a href="#">Profil</a></li>
           </ul>
         </li>
       </ul>
