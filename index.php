14e8014bdc7026102cd2677e1d5956952515738b
Amandinejude <amandine.marchand.pro@gmail.com>
2017-01-11 11:03:28 +0100

    include header

--- index.php
+++ index.php
@@ -6,15 +6,20 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="asset/css/style.css">
   </head>
+
   <body>
     <header>
-      
+      <?php 
+      include_once('inc/header.inc.php');
+       ?>
     </header>
     <main class="container">
 
     </main>
+
     <footer class="container">
 
     </footer>
+
   </body>
 </html>
