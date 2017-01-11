4e850415c698498bef268dfd717c83662fd76c1d
Amandinejude <amandine.marchand.pro@gmail.com>
2017-01-11 11:11:08 +0100

    connexion user

---
+++ inc/userConnexion.inc.php
@@ -0,0 +1,9 @@
+<?php 
+
+ ?>
+
+ <form action="index.php" method="post">
+ 	<input type="text" name="pseudo" placeholder="Votre pseudo">
+ 	<input type="password" name="password" placeholder="Votre mot de passe">
+ 	<input type="submit" value="Connexion">
+ </form>
