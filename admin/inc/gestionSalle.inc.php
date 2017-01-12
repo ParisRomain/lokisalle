b69673060a77f1ee2542e3e5552e663286fa6958
Amandinejude <amandine.marchand.pro@gmail.com>
2017-01-12 12:39:27 +0100

    masternoon

--- admin/inc/gestionSalle.inc.php
+++ admin/inc/gestionSalle.inc.php
@@ -1,36 +1,61 @@
  <!-- Affichage en tableau de la liste de salles dejà enregistrées + FORM ajout de salle -->
 
  <form>
- 	<label>Titre</label><br>
- 		<input type="text" name="titre" placeholder="titre de la salle"><br>
- 	<label>Description</label><br>
- 		<textarea placeholder="Description de la salle"></textarea>
- 	 <label>Photo</label><br>
- 		<input type="image" name="titre" value="parcourir.."> ajouter une photo<br>
-  	<label>Capacité</label><br>
- 		<?php //boucle pour afficher la capacité en menu deroulant
-		  // Variable qui ajoutera l'attribut selected de la liste déroulante
-		  $selected = '';
-		 
-		  // Parcours du tableau
-		  echo '<select name="annees">',"\n";
-		  for($i=1; $i<=500; $i++)
-		  {
-		    // Affichage de la ligne
-		    echo "\t",'<option value="', $i ,'"', $selected ,'>', $i ,'</option>',"\n";
-		    // Remise à zéro de $selected
-		    $selected='';
-		  }
-		  echo '</select>',"\n";
-		?><br>
-	<label>Catégorie</label> <br>
-		<select>
-			<option>bureau</option>
-			<option>formation</option>
-			<option>reunion</option>
-		</select>
 
+	<div class="container">
+		<div class="row">
+	
+			<section class="col-md-6">
+	 			<label>Titre</label><br>
+	 				<input type="text" name="titre" placeholder="titre de la salle"><br>
+	 			<label>Description</label><br>
+	 				<textarea name="description" placeholder="Description de la salle"></textarea><br>
+	 			 <label>Photo</label><br>
+	 				<input type="image" name="photo" value="parcourir..">	 	ajouter une photo<br>
+	  			<label>Capacité</label><br>
+	 				<?php //boucle pour afficher la capacité en menu 			deroulant
+					  // Variable qui ajoutera l'attribut selected 	de la 		liste déroulante
+					  $selected = '';
+					 
+					  // Parcours du tableau
+					  echo '<select name="capacite">',"\n";
+					  for($i=1; $i<=500; $i++)
+					  {
+					    // Affichage de la ligne
+					    echo "\t",'<option value="', $i ,'"', $selected ,'>'	, $i ,'</option>',"\n";
+					    // Remise à zéro de $selected
+					    $selected='';
+					  }
+					  echo '</select>',"\n";
+					?><br>
+				<label>Catégorie</label> <br>
+					<select name="categorie">
+						<option>bureau</option>
+						<option>formation</option>
+						<option>reunion</option>
+					</select><br>
+			</section>
 
-
+			<section class="col-md-6">
+				<label>Pays</label><br>
+					<select name="pays">
+						<option>France</option>
+					</select><br>
+			
+				<label>Ville</label><br>
+					<select name="ville">
+						<option>Lyon</option>
+						<option>Paris</option>
+					</select><br>
+			
+				<label>Adresse</label><br>
+					<textarea name="adresse" placeholder="Adresse de la salle"></textarea><br>
+			
+				<label>Code Postal</label><br>
+					<input type="text" name="cp" placeholder="Code Postal de la salle">
+			</section>
+		</div>
+	
+	</div>
 
  </form>
