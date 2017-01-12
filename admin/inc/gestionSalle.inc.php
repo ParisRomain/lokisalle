5a4d8e43a5f0b6894a74d4406010d533109af0b0
oxsparks <paris.romain.pro@gmail.com>
2017-01-12 17:30:33 +0100

    master 17h30

--- admin/inc/gestionSalle.inc.php
+++ admin/inc/gestionSalle.inc.php
@@ -1,35 +1,3 @@
- <!-- Affichage en tableau de la liste de salles dejà enregistrées + FORM ajout de salle -->
-
- <!-- <form action="../libs/services.php?action=addSalle" method="post">
-   <input type="text" name="titre" placeholder="nom de la salle">
-   <input type="text" name="description" placeholder="description">
-   <input type="file" name="photo">
-   <select name="capacite">
-     <option value="1">1</option>
-     <option value="2">2</option>
-     <option value="3">3</option>
-     <option value="4">4</option>
-     <option value="5">5</option>
-   </select>
-   <select name="categorie">
-     <option value="reunion">réunion</option>
-     <option value="formation">formation</option>
-     <option value="seminaire">séminaire</option>
-   </select>
-   <select name="pays">
-     <option value="france">France</option>
-     <option value="Japon">Japon</option>
-   </select>
-   <select name="ville">
-     <option value="paris">paris</option>
-     <option value="lyon">lyon</option>
-     <option value="marseille">marseille</option>
-   </select>
-   <input type="text" name="adresse" placeholder="Adresse">
-   <input type="text" name="cp" placeholder="Code Postal">
-   <input type="submit">
- </form> -->
- <!-- Affichage en tableau de la liste de salles dejà enregistrées + FORM ajout de salle -->
 
   <form action="../libs/services.php?action=addSalle" method="post">
 
