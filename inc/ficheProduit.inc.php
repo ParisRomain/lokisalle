a6c38275b138b99597ade13dc6c42cbaf7cbb616
Amandinejude <amandine.marchand.pro@gmail.com>
2017-01-13 15:22:37 +0100

    15:22

--- inc/ficheProduit.inc.php
+++ inc/ficheProduit.inc.php
@@ -1,6 +1,18 @@
 <?php
   include_once('header.inc.php');
- ?>
+?>
+
+<?php if(isset($_GET['salles'])){
+  $data = array('id_salle'=>$_GET['salles']);
+  $sql = "SELECT * FROM salle WHERE id_salle=:salles";
+
+  $req = $connexion->prepare($sql);
+  $req->execute($data);
+  $resultat = $req->fetch();
+
+?>
+
+ 
 
 <h1></h1>
 <button type="button" class="btn btn-success">Réserver</button>
